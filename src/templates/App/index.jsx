77425c48e39728f165979c0c0c9d2a3aa43a9da7
Gabriel <gabrielfurniel.gc@outlook.com>
2022-07-27 22:14:29 -0300

    inicio styled component

---
+++ src/templates/App/index.jsx
@@ -0,0 +1,12 @@
+import * as Styled from './styles';
+function Home() {
+  return (
+    <Styled.Wrapper>
+      <div>
+        <h1>hello</h1>
+      </div>
+    </Styled.Wrapper>
+  );
+}
+
+export default Home;
