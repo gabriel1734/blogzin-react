0e1b31ad9f210da5684b01d9fba9ea803ddf5f40
Gabriel <gabrielfurniel.gc@outlook.com>
2022-08-11 11:25:12 -0300

    teste de logo editado

--- src/components/LogoLink/LogoLink.test.jsx
+++ src/components/LogoLink/LogoLink.test.jsx
@@ -5,46 +5,7 @@ import { renderTheme } from '../../styles/render-theme';
 describe('<LogoLink />', () => {
   it('should render', () => {
     renderTheme(<LogoLink link="#" text="Logo" srcImage="Logo" />);
-    expect(screen.getByRole('heading')).toMatchInlineSnapshot(`
-      .c1 {
-        display: -webkit-box;
-        display: -webkit-flex;
-        display: -ms-flexbox;
-        display: flex;
-        -webkit-align-items: center;
-        -webkit-box-align: center;
-        -ms-flex-align: center;
-        align-items: center;
-        color: #0A1128;
-        -webkit-text-decoration: none;
-        text-decoration: none;
-      }
-
-      .c1 > img {
-        max-height: 3rem;
-      }
-
-      .c0 {
-        color: #fff;
-        font-size: 2.4rem;
-        text-transform: uppercase;
-      }
-
-      <h1
-        class="c0"
-      >
-        <a
-          class="c1"
-          href="#"
-          target="_self"
-        >
-          <img
-            alt="Logo"
-            src="Logo"
-          />
-        </a>
-      </h1>
-    `);
+    expect(screen.getByRole('heading')).toMatchSnapshot();
   });
   it('should  render text', () => {
     renderTheme(<LogoLink link="#" text="Logo" srcImage="" />);
