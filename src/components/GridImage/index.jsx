041a3ec5fe38ccf07f5a3b4dd24a6cc70e48b905
Gabriel <gabrielfurniel.gc@outlook.com>
2022-08-17 22:22:43 -0300

    grid de imagens finalizada

---
+++ src/components/GridImage/index.jsx
@@ -0,0 +1,37 @@
+import * as Styled from './styles';
+import P from 'prop-types';
+import { SectionBackground } from '../SectionBackground';
+import { Heading } from '../Heading';
+import { TextComponent } from '../TextComponent';
+
+export const GridImage = ({ title, description, grid, background = false }) => {
+  return (
+    <SectionBackground background={background}>
+      <Styled.Container>
+        <Heading size="huge" uppercase colorDark={background} as="h2">
+          {title}
+        </Heading>
+        <TextComponent>{description}</TextComponent>
+        <Styled.Grid>
+          {grid.map((el, index) => (
+            <Styled.GridElement key={index}>
+              <Styled.Image src={el.srcImg} alt={el.altText} />
+            </Styled.GridElement>
+          ))}
+        </Styled.Grid>
+      </Styled.Container>
+    </SectionBackground>
+  );
+};
+
+GridImage.propTypes = {
+  title: P.string.isRequired,
+  description: P.string,
+  grid: P.arrayOf(
+    P.shape({
+      altText: P.string.isRequired,
+      srcImg: P.string.isRequired,
+    }),
+  ).isRequired,
+  background: P.bool,
+};
